6470d80f9a0c17947ebff36cb6bcd5e1bef30304
Xiangyu Wang <xiangyu.wang@zilliz.com>
2021-03-05 18:53:39 +0800

    milestone2.py
    Signed-off-by: Xiangyu Wang <xiangyu.wang@zilliz.com>

--- milestone2.py
+++ milestone2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from milvus import Milvus, DataType, __version__
 from sklearn import preprocessing
 import numpy as np
@@ -17,13 +19,18 @@ class Test:
         self.suffix = '.npy'
         # 100,000 vector per file
         self.nfiles = 5
+        self.nvec = self.nfiles * 100000
         self.insert_bulk_size = 5000
 
     def test(self):
         try:
             # step 1 create collection
+            logging.info(f'step 1 create collection')
             if self.client.has_collection(self.cname):
+                logging.debug(f'collection {self.cname} existed')
                 self.client.drop_collection(self.cname)
+                logging.info(f'drop collection {self.cname}')
+            logging.debug(f'before create collection: {self.cname}')
             self.client.create_collection(self.cname, {
                 "fields": [{
                     "name": self.fname,
@@ -33,10 +40,12 @@ class Test:
                     "indexes": [{"metric_type": "L2"}]
                 }]
             })
+            logging.info(f'created collection: {self.cname}')
             assert self.client.has_collection(self.cname)
-            logging.info(f'after create collection: {self.cname}')
+            logging.info(f'step 1 complete')
 
             # step 2 fill data
+            logging.info(f'step 2 fill data')
             for i in range(0, self.nfiles):
                 filename = self.prefix + str(i).zfill(5) + self.suffix
                 logging.debug(f'filename: {filename}')
@@ -56,26 +65,38 @@ class Test:
             logging.info(f'after flush')
             stats = self.client.get_collection_stats(self.cname)
             logging.debug(stats)
-            # TODO: remove str() conversion
-            assert stats["row_count"] == str(self.nfiles * 100000)
-            logging.info(f'after fill collection: {self.cname}')
+            assert stats["row_count"] == self.nvec
+            logging.info(f'step 2 complete')
 
-            # step 3 load
+            # step 3 create index
+            logging.info(f'step 3 create index')
+            self.client.create_index(self.cname, self.fname, {
+                "metric_type": "L2",
+                "index_type": "IVF_FLAT",
+                "params": {"nlist": 1024}
+            })
+            logging.info(f'step 3 complete')
+
+            # step 4 load
+            logging.info(f'step 4 load')
             logging.debug(f'before load collection: {self.cname}')
             self.client.load_collection(self.cname)
-            logging.info(f'after load collection')
+            logging.info(f'step 4 complete')
 
-            # step 4 search
+            # step 5 search
+            logging.info(f'step 5 search')
             result = self.client.search(self.cname,
                                         {"bool": {"must": [{"vector": {
-                                            "Vec": {
+                                            self.fname: {
                                                 "metric_type": "L2",
                                                 "query": gen_vectors(10, self.dim),
-                                                "topk": 10
+                                                "topk": 10,
+                                                "params": {"nprobe": 10}
                                             }
                                         }}]}}
                                         )
             logging.debug(f'{result}')
+            logging.info(f'step 5 complete')
         except AssertionError as ae:
             logging.exception(ae)
         except Exception as e:
